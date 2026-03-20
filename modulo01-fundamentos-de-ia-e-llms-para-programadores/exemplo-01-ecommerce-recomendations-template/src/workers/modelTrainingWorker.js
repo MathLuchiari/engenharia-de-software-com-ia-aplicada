ba033718e9c327db37b498f3201cf03617776c9f
MathLuchiari <mm.luchiari2002@gmail.com>
2026-03-19 22:02:05 -0300

    feat: Model training

--- modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
+++ modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
@@ -3,12 +3,171 @@ import { workerEvents } from '../events/constants.js';
 
 console.log('Model training worker initialized');
 let _globalCtx = {};
+const WEIGHTS = {
+    category: 0.4,
+    color: 0.3,
+    price: 0.2,
+    age: 0.1
+}
 
+const normalize = (val, min, max) => (val - min) / ((max - min) || 1);
+
+function makeContext(products, users) {
+    const ages = users.map(u => u.age);
+    const prices = products.map(p => p.price);
+
+    const minAge = Math.min(...ages);
+    const maxAge = Math.max(...ages);
+
+    const minPrice = Math.min(...prices);
+    const maxPrice = Math.max(...prices);
+
+    const colors = [...new Set(products.map(p => p.color))];
+    const categories = [...new Set(products.map(p => p.category))];
+
+    const colorsIndex = Object.fromEntries(
+        colors.map((color, index) => {
+            return [color, index];
+        })
+    )
+
+    const categoriesIndex = Object.fromEntries(
+        categories.map((category, index) => {
+            return [category, index];
+        })
+    )
+
+    //Computar a média de idade dos compradores por idade
+    // Ajuda a personalizar
+    const midAge = (minAge + maxAge) / 2;
+    const ageSums = {};
+    const ageCounts = {};
+
+    users.forEach(user => {
+        user.purchases.forEach(p => {
+            ageSums[p.name] = (ageSums[p.name] || 0) + user.age;
+            ageCounts[p.name] = (ageCounts[p.name] || 0) + 1;
+        })
+    })
+
+    const productAvgAgeNorm = Object.fromEntries(
+        products.map(product => {
+            const avgAge = ageCounts[product.name] ? 
+                ageSums[product.name] / ageCounts[product.name] : 
+                midAge;
+
+                return [product.name, normalize(avgAge, minAge, maxAge)];
+        })
+    )
+
+    return {
+        products,
+        users,
+        colorsIndex,
+        categoriesIndex,
+        minAge,
+        maxAge,
+        minPrice,
+        maxPrice,
+        productAvgAgeNorm,
+        numCategories: categories.length,
+        numColors: colors.length,
+        dimensions: 2 + categories.length + colors.length
+    };
+}
+
+const oneHotWeighted = (index, length, weight) => tf.oneHot(index, length).cast('float32').mul(weight);
+
+function encodeProduct(product, context) {
+    //Nornalizando dados para ficar de 0 a 1 e aplicar o peso na recomentdação
+    const price = tf.tensor1d([
+        normalize(product.price, context.minPrice, context.maxPrice) * WEIGHTS.price
+    ])
+
+    const age = tf.tensor1d([
+        (context.productAvgAgeNorm[product.name] ?? 0.5) * WEIGHTS.age
+    ])
+
+    const category = oneHotWeighted(
+        context.categoriesIndex[product.category],
+        context.numCategories,
+        WEIGHTS.category
+    )
+    
+    const color = oneHotWeighted(
+        context.colorsIndex[product.color],
+        context.numColors,
+        WEIGHTS.color
+    )
+
+    return tf.concat1d(
+        [price, age, category, color]
+    );
+}
+
+function encodeUser(user, context) {
+    if(user.purchases.length) {
+        return tf.stack(
+            user.purchases.map(
+                p => encodeProduct(p, context)
+            )
+        )
+        .mean(0)
+        .reshape([
+            1,
+            context.dimensions
+        ])
+    }
+}
+
+function createTrainingData( context ) {
+    const inputs = [];
+    const labels = [];
+
+    context.users.forEach(user => {
+        const userVector = encodeUser(user, context).dataSync();
+        context.products.forEach(product => {
+            const productVector = encodeProduct(product, context).dataSync();
+
+            const label = user.purchases.some(purchase => purchase.name === product.name ? 1 : 0);
+
+            //Combinar user + product
+            inputs.push([...userVector, ...productVector]);
+            labels.push(label);
+        })
+
+    })
+    
+    return {
+        xs: tf.tensor2d(inputs),
+        ys: tf.tensor2d(labels, [labels.length, 1]),
+        inputDimensions: context.dimensions * 2
+        //tamanho = userVector + productVector
+    }
+}
 
 async function trainModel({ users }) {
     console.log('Training model with users:', users)
 
     postMessage({ type: workerEvents.progressUpdate, progress: { progress: 50 } });
+    const products = await(await fetch('/data/products.json')).json();
+
+    const context = makeContext(products, users);
+    context.productVectors = products.map( product => {
+        return {
+            name: product.name,
+            meta: {...product},
+            vector: encodeProduct(product, context).dataSync()
+        }
+
+    })
+
+    _globalCtx = context;
+
+    const trainingData = createTrainingData(context);
+
+    debugger;
+
     postMessage({
         type: workerEvents.trainingLog,
         epoch: 1,
